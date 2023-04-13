a71db4b8fe58fede00596cfa70c003777b1ca1eb
iamyunusali <aliyunus291@gmail.com>
2023-04-13 10:51:10 +0300

    add functionalut to add course button

---
+++ script.js
@@ -0,0 +1,55 @@
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const course = document.querySelector(".course-form");
+const factsList = document.querySelector(".course-lists");
+
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+// Load data from Supabase
+loadFacts();
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://cluwenguxyodwixzwnxh.supabase.co/rest/v1/courses",
+    {
+      headers: {
+        apikey: "",
+        authorization: "",
+      },
+    }
+  );
+  const data = await res.json();
+  createFactsList(data);
+}
+
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
